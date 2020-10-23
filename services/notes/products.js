e1a0922358e8c0299b8cab0028b28f118cf06210
Cerealizable <alberstc1@gmail.com>
2020-10-23 09:38:31 -0700

    changed productList function to accept companyId

--- services/notes/products.js
+++ services/notes/products.js
@@ -60,7 +60,7 @@ export const list = handler(async (event, context) => {
     // - ':userId': defines 'userId' to be Identity Pool identity id of the authenticated user
     KeyConditionExpression: "companyId = :companyId",
     ExpressionAttributeValues: {
-      ":companyId": event.requestContext.identity.cognitoIdentityId
+      ":companyId": event.pathParameters.companyId
     }
   };
   const result = await dynamoDb.query(params);
