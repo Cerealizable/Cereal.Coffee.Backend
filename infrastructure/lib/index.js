a629b1accafc176c012f5f961864941e392171f2
Cerealizable <alberstc1@gmail.com>
2020-10-23 12:12:56 -0700

    renaming file lookup

--- infrastructure/lib/index.js
+++ infrastructure/lib/index.js
@@ -1,7 +1,7 @@
 import S3Stack from "./S3Stack";
 import CognitoStack from "./CognitoStack";
 import DynamoDBStack from "./DynamoDBStack";
-import Product_DynamoDBStack from "./Product_DynamoDBStack";
+import Product_DynamoDBStack from "./Product_DynamodbStack";
 
 
 // Add stacks
